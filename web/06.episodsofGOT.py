67b357dbc4c9d543d9912c9b93753b95b3b49164
mr-moshtaghi <mr.ahmadalimoshtaghi@gmail.com>
2021-01-14 22:56:59 +0330

    episodesofGOT

---
+++ web/06.episodsofGOT.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://en.wikipedia.org/wiki/List_of_Game_of_Thrones_episodes'
+r = requests.get(url)
+
+content = BeautifulSoup(r.text , 'html.parser')
+
+episodes = []
+# EPTable = content.findAll('table' , 'wikiepisodetable')
+EPTable = content.select('table.wikiepisodetable')[:8]
+
+for table in EPTable:
+    headers = []
+    rows = table.findAll('tr')
+    for header in table.find('tr').findAll('th'):
+        headers.append(header.text)
+    for row in table.findAll('tr')[1:]:
+        values=[]
+        for col in row.findAll(['th' , 'td']):
+            values.append(col.text)
+        if values:
+            episodeDict = {headers[i] : values[i] for i in range(len(values))}
+            episodes.append(episodeDict)
+
+for ep in episodes:
+    print(ep)
