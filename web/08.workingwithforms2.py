89004abf8b2d0c121fdead6ea9dd8e16c4d0ca48
mr-moshtaghi <mr.ahmadalimoshtaghi@gmail.com>
2021-01-15 09:59:43 +0330

    protection in metode post

---
+++ web/08.workingwithforms2.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://www.webscrapingfordatascience.com/postform3/'
+
+r = requests.get(url)
+html = BeautifulSoup(r.text , 'html.parser')
+protection = html.find('input' , {'name':'protection'}).get('value')
+
+formsData = {
+    'name':'sajad',
+    'gender':'F',
+    'pazza':'like',
+    'commends':'i am sajad',
+    'protection':protection
+}
+
+result = requests.post(url , data=formsData)
+
+print(result.text)
