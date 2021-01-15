3100d3e5bab16e11dc18d63a35105b34237d730b
mr-moshtaghi <mr.ahmadalimoshtaghi@gmail.com>
2021-01-15 21:29:09 +0330

    explicit wait

---
+++ web/16.explicitwait.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+driver.get('http://www.webscrapingfordatascience.com/complexjavascript/')
+
+quoteElements = WebDriverWait(driver , 10).until(
+    EC.presence_of_all_elements_located(
+        (By.CLASS_NAME, 'quate')
+        # (By.CSS_SELECTOR, '.quote')
+    )
+)
+
+for q in quoteElements:
+    print(q.text)
